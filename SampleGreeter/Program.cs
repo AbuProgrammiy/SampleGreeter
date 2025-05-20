104ad504ce772932d10bd3b4a517bdf5fb381baa
Abu_Programmiy <abdukholiq0907@gmail.com>
2025-05-20 17:03:09 +0500

    bot example

--- SampleGreeter/Program.cs
+++ SampleGreeter/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace SampleGreeter
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("6526535333:AAHXVFBwruT-4zsnhbI_h2cVTRJXRr9cA7g", cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel(); // stop the bot
+
+// method that handle messages received by the bot:
+async Task OnMessage(Message msg, UpdateType type)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    if (msg.Text is null) return;	// we only handle Text messages here
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+    // let's echo back received text in the chat
+    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
 }
